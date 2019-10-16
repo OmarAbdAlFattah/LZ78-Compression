957a099c5f6bdf5312b9d96997b3faed3d160c8a
Omar Abd-Al-Fattah <omarspps5@gmail.com>
2019-10-16 16:37:36 +0200

    Update Compression.java

--- Compression.java
+++ Compression.java
@@ -10,20 +10,22 @@ public class Compression {
 		{
 			reading = Character.toString(input.charAt(i));
 
-			if(dicClass.inDic(reading, dicClass.dic) == 0)		//if not found
+			if(dicClass.inDic(reading, dicClass.dic) == -1)		//if not found
 			{
 				dicClass.dic.add(reading);	//add in dictionary
-				//System.out.println(dicClass.dic.get(i));
 				System.out.println("the dictionary contains: " + dicClass.dic);
-				encryption.put(index, input.charAt(i));    //add in final compressed result
-				index = i;
+				encryption.put(i, input.charAt(i));    //add in final compressed result
+				System.out.println("the encryption contains: " + encryption);
+				System.out.println();
 			}
 			
-			else
+			else		//found already
 			{
-				String theTemp = formulate(input, reading, i, dicClass);
-				
-				dicClass.dic.add(theTemp);
+				//System.out.println("i in compress is: " + i);
+				String theTemp_value = formulate(input, reading, i, dicClass).get(i);
+				//System.out.println("the temp value is " + theTemp_value);
+				// i is the index where the first element isn't found in dictionary
+				dicClass.dic.add(theTemp_value);
 				encryption.put(index, input.charAt(i));
 			}
 			//i = index;
@@ -32,23 +34,25 @@ public class Compression {
 		
 	}
 	
-	String formulate(String input, String reading, int index, Dictionary dictionary)	              
+	String[] temp = new String [1];		//to save the updated value of temp to use out side the loop
+					//INDEX: we need to know the index to add it to the compression
+	Map<Integer, String> formulated = new HashMap<>(1);
+	int counter = 0;
+	Map<Integer, String> formulate(String input, String reading, int index, Dictionary dictionary)	              
 	{
-		String[] temp;		//to save the updated value of temp to use out side the loop
-		temp = new String[1];
-		
-		int j = index;
-		while(j < input.length() - 1)
-		{
-			temp[0] =  merge(reading, Character.toString(input.charAt(index + j)));
-			System.out.println("STUCK IN WHILE LOOP NOT RECURSION");
-			if(dictionary.inDic( temp[0], dictionary.dic) == 0){	//found
-				formulate(input, temp[0], index, dictionary);
-			}
-			else
-				break;
+		//temp[0] = reading;
+		if(dictionary.inDic( reading, dictionary.dic) != -1 && index < input.length() - 1){//found
+			reading = merge(reading, Character.toString(input.charAt(index + 1)));
+			//System.out.println("the current reading: " + reading);
+			formulated.put(index + 1, reading);	// 3=bc
+			//System.out.println("index is : " + index);
+			index ++;
+			//System.out.println("Print is -> " + reading);
+			//System.out.println("modified -> " + reading);
+			formulate(input, reading, index, dictionary);
 		}
-		return temp[0];
+		
+		return formulated;
 	}
 	
 	
@@ -81,9 +85,5 @@ public class Compression {
 		
 	}
 	return fullInput;
-}*/	
-	
-	
-	
-	
+	}*/	
 }
